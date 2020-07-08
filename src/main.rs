87e5f35258b19a9b50704b39bc65cae8384b6329
Peter Bui <btxtruong@gmail.com>
2020-07-09 00:51:27 +0700

    Just ping

--- src/main.rs
+++ src/main.rs
@@ -19,22 +19,26 @@ impl RedisCli {
         self.stream.flush().unwrap();
     }
 
-    fn receive(&mut self) -> String {
+    fn receive(&mut self) {
         let mut buffer = [0; 512];
         self.stream.read(&mut buffer).unwrap();
-        if let Cow::Owned(data) = String::from_utf8_lossy(&buffer[..]) {
-            return data;
-        }
-        "abs".to_string()
+        println!("{}", String::from_utf8_lossy(&buffer[..]));
+        // if let Cow::Owned(data) = String::from_utf8_lossy(&buffer[..]) {
+        //     return data;
+        // }
+
+        // "wrong".to_string()
     }
 }
 
 fn main() {
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).expect("Error reading input");
-    let mut input: Vec<&str> = input.split_whitespace().collect();
+    loop {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).expect("Error reading input");
+        let mut input: Vec<&str> = input.split_whitespace().collect();
 
-    let mut redis = RedisCli::connect("127.0.0.1", 6379);
-    redis.send(input.join("").as_bytes());
-    println!("{}", redis.receive())
+        let mut redis = RedisCli::connect("127.0.0.1", 6379);
+        redis.send(format!("{}\n", input.join("")).as_bytes());
+        redis.receive();
+    }
 }
